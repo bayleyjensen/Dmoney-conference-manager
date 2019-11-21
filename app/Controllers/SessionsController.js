e8e2e9ddb0c0918865eea1e610190913ee77b4bc
bayleyjensen <bayley3jensen0@gmail.com>
2019-11-21 14:35:52 -0700

    copy over

--- app/Controllers/SessionsController.js
+++ app/Controllers/SessionsController.js
@@ -1,40 +1,40 @@
 import SessionsService from "../Services/SessionsService.js";
-import _store from "../store.js"
+import _store from "../store.js";
 
 // private
 function _drawSessions() {
   // get html
 
- let template = ''
- let sessions = _store.State.sessions
- sessions.forEach(session => template += session.template)
- document.querySelector("#sessions").innerHTML = template
+  let template = "";
+  let sessions = _store.State.sessions;
+  sessions.forEach(session => (template += session.template));
+  document.querySelector("#sessions").innerHTML = template;
 }
 
 // public
 export default class SessionsController {
   constructor() {
     _drawSessions();
-    console.log('Hello from sesionsController')
+    console.log("Hello from sesionsController");
   }
 
-  addSpeaker(event, sessionId){
-    event.preventDefault()
-    let formData = event.target
+  addSpeaker(event, sessionId) {
+    event.preventDefault();
+    let formData = event.target;
 
     let newSpeaker = {
       name: formData.name.value,
       topic: formData.topic.value,
       time: formData.time.value,
       sessionId
-    }
+    };
 
-    SessionsService.addSpeaker(newSpeaker)
-    formData.reset()
-    _drawSessions()
+    SessionsService.addSpeaker(newSpeaker);
+    formData.reset();
+    _drawSessions();
   }
-  deleteSpeaker (sessionId, speakerId) {
-    SessionsService.deleteSpeaker(sessionId, speakerId)
-    _drawSessions()
+  deleteSpeaker(sessionId, speakerId) {
+    SessionsService.deleteSpeaker(sessionId, speakerId);
+    _drawSessions();
   }
 }
